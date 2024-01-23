02650d246a858daf6a08384a5ae3e8eeeb0054fc
Sanskar-12 <sanskarv2004@gmail.com>
2024-01-24 00:23:06 +0530

    done

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Chai = () => {
+  return (
+    <div className='h-full flex justify-center items-center flex-col'>
+      <div>Chadcn</div>
+      <Button variant={"chai"}>Chai aur code</Button>
+      <Button variant={"chai"}>Chai aur code</Button>
+    </div>
+  )
+}
+
+export default Chai
